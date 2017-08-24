a27f968c05a56f0f3c9c1a1c548cac0905571b57
Omer Levi Hevroni <omerl@soluto.com>
2017-08-24 15:29:50 +0300

    use get_plugin in test and plugin for demonstration

--- src/plugin-sample.php
+++ src/plugin-sample.php
@@ -4,6 +4,12 @@
  * Plugin Name: Sample Plugin
  */
 
+// Check if get_plugins() function exists. This is required on the front end of the
+// site, since it is in a file that is normally only loaded in the admin.
+if ( ! function_exists( 'get_plugins' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
 // scheduled email to admin
 register_activation_hook( __FILE__, 'plugin_sample_activation' );
 /**
@@ -13,5 +19,5 @@ function plugin_sample_activation() {
 }
 
 function some_function(){
-	return 4;
+	return count(get_plugins());
 }
--- tests/test-plugin-sample.php
+++ tests/test-plugin-sample.php
@@ -14,8 +14,9 @@ class SampleTests extends WP_UnitTestCase {
 	 * @test
 	 */
 	function some_function_return_4() {
+		$expected = count(get_plugins());
 		$result = some_function();
 
-		$this->assertSame(4, $result);
+		$this->assertSame($expected, $result);
 	}
 }
